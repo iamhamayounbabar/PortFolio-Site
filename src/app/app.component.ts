ec44f7704fc61336851db986697348392ce9c570
iamhamayounbabar <iamhamayounbabar@gmail.com>
2024-05-31 06:46:04 +0500

    add components & faveicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./components/home-page/home-page.component";
+import { ServicesComponent } from './components/services/services.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomePageComponent]
+    imports: [RouterOutlet, HomePageComponent, ServicesComponent]
 })
 export class AppComponent {
   title = 'HB-Portfolio-Site';
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { ServicesComponent } from "../services/services.component";
+import { MySkillsComponent } from "../my-skills/my-skills.component";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [NavbarComponent]
+    imports: [NavbarComponent, ServicesComponent, MySkillsComponent]
 })
 export class HomePageComponent {
 

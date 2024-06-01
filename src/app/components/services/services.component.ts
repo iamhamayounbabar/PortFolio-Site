1e563ccef0ee2d07c0ebd311d20b65f9f2ad8841
iamhamayounbabar <iamhamayounbabar@gmail.com>
2024-06-01 09:16:26 +0500

    add carousel and services components

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CarouselComponent } from "../carousel/carousel.component";
 
 @Component({
-  selector: 'app-services',
-  standalone: true,
-  imports: [],
-  templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+    selector: 'app-services',
+    standalone: true,
+    templateUrl: './services.component.html',
+    styleUrl: './services.component.css',
+    imports: [CarouselComponent]
 })
 export class ServicesComponent {
 

cbc6d1bf1246cc0a4d107e6a0e9f2cc1c2193a5b
Iamumairali <umairjutt7703@gmail.com>
2024-06-01 13:50:02 +0500

    update site

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { ServicesComponent } from "../services/services.component";
 import { MySkillsComponent } from "../my-skills/my-skills.component";
+import { DetailsComponent } from '../details/details.component';
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [NavbarComponent, ServicesComponent, MySkillsComponent]
+    imports: [NavbarComponent, ServicesComponent, MySkillsComponent, DetailsComponent]
 })
 export class HomePageComponent {
 
